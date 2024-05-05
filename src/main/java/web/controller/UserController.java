84e6bc45d1215f140661bb46956bbfc9f51a3b61
1vsALL <alikkv@mail.ru>
2024-05-05 20:52:51 +0300

    create CRUD

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,10 +1,16 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
-
+    @GetMapping()
+    public String users(ModelMap modelMap) {
+        
+        return "users";
+    }
 }
--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -1,15 +1,34 @@
 package web.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column
     private String name;
+    @Column
     private String lastName;
+    @Column
     private int age;
 
     public User(String name, String lastName, int age) {
         this.name = name;
         this.lastName = lastName;
         this.age = age;
+    }
+
+    public User() {
+
     }
 
     public String getName() {
